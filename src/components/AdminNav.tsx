b7c039538379c9fe97417088c85b8adebc506e8d
benjamincherriere <bcherriere@plus-de-bulles.com>
2025-08-09 16:43:31 +0200

    feat: add admin navigation component

--- src/components/AdminNav.tsx
+++ src/components/AdminNav.tsx
@@ -1,1 +1,19 @@
-// Barre de navigation admin pour MANAGER
+import { Link } from 'react-router-dom'
+
+function AdminNav() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/admin/products">Produits</Link>
+        </li>
+        <li>
+          <Link to="/admin/meetings">Rendez-vous</Link>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default AdminNav
+
